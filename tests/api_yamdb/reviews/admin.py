22c6ca81c3d13d47812a3bafb79a4e0500267acb
YOUR_NAME <YOUR_EMAIL>
2023-03-30 23:09:41 +0300

    30.03(7)

--- tests/api_yamdb/reviews/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Category, Comments, Genre, Review, Title
-
-admin.site.register(Category)
-admin.site.register(Comments)
-admin.site.register(Genre)
-admin.site.register(Review)
-admin.site.register(Title)
